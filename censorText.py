da28b565fd84a513046bd76140d9975062fbc89b
Spyros Magripis <mechoulas@gmail.com>
2016-01-18 21:07:29 +0100

    Update censorText.py
    added parenthesis to avoid error in 3.4

--- censorText.py
+++ censorText.py
@@ -1,15 +1,15 @@
 def censor(text,word):
     splitText = text.split()
-    print "1: ",splitText
+    print("1: ",splitText)
     for index,item in enumerate(splitText):
         if item == word:
             item = "*" * len(item)
             splitText[index]= item
             #i+=1
-            print "2: ", item
+            print("2: ", item)
             
     mergedText = " ".join(splitText)
-    print "3: ",mergedText
+    print("3: ",mergedText)
     return mergedText
     
     
